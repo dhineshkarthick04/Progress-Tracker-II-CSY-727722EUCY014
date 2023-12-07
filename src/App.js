35566f760501f7b6802900835d0185435cff656a
dhineshkarthick04 <149822642+dhineshkarthick04@users.noreply.github.com>
2023-12-07 17:12:11 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from "react";
-import Put from "./Axios/Put";
-function App() {
-  return (
-    <div>
-      <Put />
-    </div>
+import React,{useState} from 'react'
+import axios from 'axios';
+
+function App(){
+  const [state,setState]=useState([]);
+  axios.get("https://jsonplaceholder.typicode.com/albums")
+  .then((response)=>{
+    const data=response.data;
+    setState(data);
+    console.log(data[0].title);
+  })
+  return(
+    <ul>
+    {
+      state.map((album)=>(
+        <li key={album.id}>{album.title}</li>
+      ))
+    }</ul>
   );
 }
 
